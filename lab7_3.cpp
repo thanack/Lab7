1087ab8b88bd264305f4cccf4de4e782c0b69879
thanack <thanaphat2543@gmail.com>
2020-12-29 16:46:50 +0700

    lab7_3.cpp

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,11 +1,38 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your property: "
-"Your status = "
-"UNFRIEND"
-"FRIEND"
-"BEST FRIEND"
-"ONE-NIGHT-STAND"
-"MARRIED"
-*/
+#include<iostream>
+using namespace std;
+int main(){
+	int ages,pp;
+	string states;
+	double hights;
+	cout<<"Enter your age: ";
+	cin>>ages;
+	if(ages<=20){
+		cout<<"Enter your height: ";
+		cin>>hights;
+		if(hights<160){
+			states="UNFRIEND";
+		}else if(hights<175){
+			states="FRIEND";
+		}else{
+			cout<<"Enter your property: ";
+		     cin>>pp;
+		     if(pp>69000000){
+		     	states="MARRIED";
+			 }else{
+			 	states="ONE-NIGHT-STAND";
+			 }
+		}
+	}else if(ages<30){
+		cout<<"Enter your property: ";
+		cin>>pp;
+	      if(pp>10000000){
+	      	states="BEST FRIEND";
+		  }else{
+		  	states="UNFRIEND";
+		  }
+	}else{
+		states="UNFRIEND";
+	}
+	cout<<"Your status = "<<states;
+	return 0;
+}
